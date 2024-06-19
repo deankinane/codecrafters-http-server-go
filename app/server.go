abf872f6515e7ca1bcb5685c73866654efd93f49
DeanK <deankinane@proton.me>
2024-06-19 12:37:41 +0100

    return a file

--- app/server.go
+++ app/server.go
@@ -9,9 +9,15 @@ import (
 	"strings"
 )
 
+var param_directory string = ""
+
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
+
+	if len(os.Args) > 2 {
+		param_directory = os.Args[2]
+	}
 
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
@@ -26,7 +32,7 @@ func main() {
 			return
 		}
 
-		HandleConnection(conn)
+		go HandleConnection(conn)
 	}
 
 }
@@ -41,27 +47,51 @@ func HandleConnection(conn net.Conn) {
 	if strings.HasPrefix(req.URL.Path, "/echo/") {
 		fmt.Println(req.URL.Path)
 		echo := strings.Replace(req.URL.Path, "/echo/", "", 1)
-		conn.Write([]byte(BuildResonseBody(200, echo)))
+		conn.Write([]byte(BuildResponseText(200, echo)))
 	} else if req.URL.Path == "/user-agent" {
-		conn.Write([]byte(BuildResonseBody(200, req.UserAgent())))
+		conn.Write([]byte(BuildResponseText(200, req.UserAgent())))
+	} else if strings.HasPrefix(req.URL.Path, "/files/") {
+		path := strings.Replace(req.URL.Path, "/files/", "", 1)
+		conn.Write(([]byte(ServeFile(path))))
 	} else if req.URL.Path == "/" {
-		conn.Write([]byte(BuildResonseBody(200, "")))
+		conn.Write([]byte(EmptyResponse(200)))
 	} else {
-		conn.Write([]byte(BuildResonseBody(404, "")))
+		conn.Write([]byte(EmptyResponse(404)))
 	}
 }
 
-func BuildResonseBody(code int, body string) string {
+func ServeFile(path string) string {
+	content, err := os.ReadFile(param_directory + "/" + path)
+	if err != nil {
+		return EmptyResponse(404)
+	}
+
+	return BuildResponseBinary(200, content)
+}
+
+func BuildResponseBody(code int, content_type string, content_length int, body string) string {
 	response := ResponseCode(code)
 
 	if len(body) > 0 {
-		response += "Content-Type: text/plain\r\n"
-		response += "Content-Length: " + fmt.Sprint(len(body)) + "\r\n"
+		response += "Content-Type: " + content_type + "\r\n"
+		response += "Content-Length: " + fmt.Sprint(content_length) + "\r\n"
 	}
 
 	response += "\r\n"
 	response += body
 	return response
+}
+
+func BuildResponseText(code int, body string) string {
+	return BuildResponseBody(code, "text/plain", len(body), body)
+}
+
+func BuildResponseBinary(code int, body []byte) string {
+	return BuildResponseBody(code, "application/octet-stream", len(body), string(body))
+}
+
+func EmptyResponse(code int) string {
+	return BuildResponseText(code, "")
 }
 
 func ResponseCode(code int) string {
