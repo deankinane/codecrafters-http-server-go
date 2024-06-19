cb467f5c4c2c69ede7585d081e3c35be5de3f2e0
DeanK <deankinane@proton.me>
2024-06-19 11:54:07 +0100

    read user-agent

--- app/server.go
+++ app/server.go
@@ -38,16 +38,21 @@ func HandleConnection(conn net.Conn) {
 	if strings.HasPrefix(req.URL.Path, "/echo/") {
 		fmt.Println(req.URL.Path)
 		echo := strings.Replace(req.URL.Path, "/echo/", "", 1)
-		response := "HTTP/1.1 200 OK\r\n"
-		response += "Content-Type: text/plain\r\n"
-		response += "Content-Length: " + fmt.Sprint(len(echo)) + "\r\n"
-		response += "\r\n"
-		response += echo
-		conn.Write([]byte(response))
+		conn.Write([]byte(BuildResonseBody(echo)))
+	} else if req.URL.Path == "/user-agent" {
+		conn.Write([]byte(BuildResonseBody(req.UserAgent())))
 	} else if req.URL.Path == "/" {
 		conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
 	} else {
 		conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
 	}
+}
 
+func BuildResonseBody(body string) string {
+	response := "HTTP/1.1 200 OK\r\n"
+	response += "Content-Type: text/plain\r\n"
+	response += "Content-Length: " + fmt.Sprint(len(body)) + "\r\n"
+	response += "\r\n"
+	response += body
+	return response
 }
