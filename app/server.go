d917f279079c98f7b6e5b0c825784cf5436e0930
DeanK <deankinane@proton.me>
2024-06-19 11:42:52 +0100

    response body 2

--- app/server.go
+++ app/server.go
@@ -44,6 +44,8 @@ func HandleConnection(conn net.Conn) {
 		response += "\r\n"
 		response += echo
 		conn.Write([]byte(response))
+	} else {
+		conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
 	}
 
 }
