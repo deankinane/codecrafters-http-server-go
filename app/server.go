9634870aa72c373e78470223a5a8a251dfecb5ff
DeanK <deankinane@proton.me>
2024-06-19 12:03:28 +0100

    refactor

--- app/server.go
+++ app/server.go
@@ -38,21 +38,39 @@ func HandleConnection(conn net.Conn) {
 	if strings.HasPrefix(req.URL.Path, "/echo/") {
 		fmt.Println(req.URL.Path)
 		echo := strings.Replace(req.URL.Path, "/echo/", "", 1)
-		conn.Write([]byte(BuildResonseBody(echo)))
+		conn.Write([]byte(BuildResonseBody(200, echo)))
 	} else if req.URL.Path == "/user-agent" {
-		conn.Write([]byte(BuildResonseBody(req.UserAgent())))
+		conn.Write([]byte(BuildResonseBody(200, req.UserAgent())))
 	} else if req.URL.Path == "/" {
-		conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+		conn.Write([]byte(BuildResonseBody(200, "")))
 	} else {
-		conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+		conn.Write([]byte(BuildResonseBody(404, "")))
 	}
 }
 
-func BuildResonseBody(body string) string {
-	response := "HTTP/1.1 200 OK\r\n"
-	response += "Content-Type: text/plain\r\n"
-	response += "Content-Length: " + fmt.Sprint(len(body)) + "\r\n"
+func BuildResonseBody(code int, body string) string {
+	response := ResponseCode(code)
+
+	if len(body) > 0 {
+		response += "Content-Type: text/plain\r\n"
+		response += "Content-Length: " + fmt.Sprint(len(body)) + "\r\n"
+	}
+
 	response += "\r\n"
 	response += body
 	return response
+}
+
+func ResponseCode(code int) string {
+	codeStr := ""
+	switch code {
+	case 200:
+		codeStr = "200 OK"
+	case 404:
+		codeStr = "404 Not Found"
+	default:
+		codeStr = "400 Bad Request"
+	}
+
+	return "HTTP/1.1 " + codeStr + "\r\n"
 }
